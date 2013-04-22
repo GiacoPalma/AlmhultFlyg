f44a7f91a84ae053b08ee317e9c9eecd4adffe63
Lukas Andersson <lukas_andersson@live.se>
2013-04-22 12:39:15 +0200

    döpte om string till stringa

--- src/MainClass.java
+++ src/MainClass.java
@@ -8,7 +8,7 @@ public class MainClass {
 		// TODO Auto-generated method stub
 		String string = "Test";
 		System.out.println(string);
-		String string ="testbranch";
+		String stringa ="testbranch";
 	}
 
 }
