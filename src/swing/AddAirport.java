1ee2bd28fe58a666e94fa998859af9ea6bbf7662
Viktor Johannisson <viktor.johannisson@gmail.com>
2013-04-24 09:20:03 +0200

    removed exit on close, added hide on close

--- src/swing/AddAirport.java
+++ src/swing/AddAirport.java
@@ -38,7 +38,7 @@ public class AddAirport extends JFrame {
 	 * Create the frame.
 	 */
 	public AddAirport() {
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setDefaultCloseOperation(JFrame.HIDE_ON_CLOSE);
 		setBounds(100, 100, 450, 300);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
